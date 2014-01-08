2ae54e4e3b48b3c9e7d383de43a887b501338f00
vvator <vvator@gmail.com>
2014-01-08 16:42:13 +0300

    14.01.2014 work NEW

--- CT_CONTRACT/src/vva/contract/data/servlet/DataServletIB.java
+++ CT_CONTRACT/src/vva/contract/data/servlet/DataServletIB.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+import sun.util.resources.CalendarData;
 import vva.contract.data.db.interbase.IBRequest;
 
 /**
@@ -37,6 +37,7 @@ public class DataServletIB extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setHeader("Cache-Control", "no-cache");
 		/////////////////////////////////////////////////////////////////////
 		/// set content encoding for response
 		/////////////////////////////////////////////////////////////////////
@@ -147,7 +148,19 @@ System.out.println("============================================================
 		
 		try {
 			IBRequest ibRequest = new IBRequest();
-			ibRequest.getAllContractsWithFilter_IB(start,limit);
+			ibRequest.getAllContractsWithFilter_IB( start,limit,
+													"",//"F_FULLNAMEDOG",
+													"",//"F_RESULTDOG",
+													"",//"F_RIGHTHOLDER",
+													"",//"F_SUBDOG_INPROCESS",
+													"",//"F_SUBDOG_PRED",
+													"",//"F_PERFORMER",
+													""//"F_COUNTERAGENT_FNAME"
+													);
+			
+			//IBRequest ibReqPerformer = new IBRequest();
+			//ibReqPerformer.getAllPerformerWithFilter_IB(start,limit);
+			
 			JSONArray arrayObj = new JSONArray();
 			while (ibRequest.rs.next()){
 				
@@ -155,13 +168,17 @@ System.out.println("============================================================
 				
 				JSONObject jon = new JSONObject();
 					jon.put("I", ibRequest.rs.getString("i"));
-					jon.put("ID_DOG", fPerformer+ibRequest.rs.getString("ID_DOG"));
-					jon.put("KONTROL", fCustomer+ ibRequest.rs.getString("KONTROL"));
-					jon.put("ID_COUNT",fRightholder+ ibRequest.rs.getString("ID_COUNT"));
+					jon.put("ID_DOG", ibRequest.rs.getString("ID_DOG"));
+					jon.put("KONTROL", ibRequest.rs.getString("KONTROL"));
+					jon.put("ID_COUNT", ibRequest.rs.getString("ID_COUNT"));
 					jon.put("ID_ACCOUNT", ibRequest.rs.getString("ID_ACCOUNT"));
 					jon.put("NAMEDOG", ibRequest.rs.getString("NAMEDOG"));
 					jon.put("NOM_DOG", ibRequest.rs.getString("NOM_DOG"));
-					jon.put("DATA_W", ibRequest.rs.getString("DATA_W"));
+					
+					
+					SimpleDateFormat sdf_DATA_W = new SimpleDateFormat("dd.mm.yyyy");
+					jon.put("DATA_W", sdf_DATA_W.format(ibRequest.rs.getDate("DATA_W")));
+					
 					jon.put("SROK_PLAT", ibRequest.rs.getString("SROK_PLAT"));
 					jon.put("SUMMA", ibRequest.rs.getString("SUMMA"));
 					jon.put("CURRENCY", ibRequest.rs.getString("CURRENCY"));
@@ -175,11 +192,39 @@ System.out.println("============================================================
 					//jon.put("KOD1C", ibRequest.rs.getString("KOD1C"));
 					jon.put("DATE_STATUS", ibRequest.rs.getString("DATE_STATUS"));
 					jon.put("REG_NOM", ibRequest.rs.getString("REG_NOM"));
-					jon.put("REG_DATE", ibRequest.rs.getString("REG_DATE"));
+					
+					SimpleDateFormat sdf_REG_DATE = new SimpleDateFormat("dd.mm.yyyy");
+					jon.put("REG_DATE", sdf_DATA_W.format(ibRequest.rs.getDate("REG_DATE")));
+
 					jon.put("REG_SECT", ibRequest.rs.getString("REG_SECT"));
 					jon.put("REG_INDEX", ibRequest.rs.getString("REG_INDEX"));
 					jon.put("FULL_NUM", ibRequest.rs.getString("FULL_NUM"));
-					jon.put("USER_FIO1", ibRequest.rs.getString("USER_FIO1"));				
+					jon.put("USER_FIO1", ibRequest.rs.getString("USER_FIO1"));		
+
+																						//System.out.print(ibRequest.rs.getString("ID_DOG")+"[");
+																						/*	String performers="";	
+																							
+																							//ibReqPerformer.rs(TYPE_SCROLL_INSENSITIVE);
+																							//ibReqPerformer.rs.next();
+																									while(ibReqPerformer.rs.next() 
+																										 )
+																									{	
+																										if(ibReqPerformer.rs.getString("id_dog").equals(ibRequest.rs.getString("id_dog"))){
+																									      performers+=ibReqPerformer.rs.getString("performer")+ "<br>";
+																									      System.out.print(ibReqPerformer.rs.getString("id_dog")+";");
+																									}
+																									}		
+																									
+																				System.out.println("]");
+																						 */
+
+					
+					jon.put("counteragent_fname",ibRequest.rs.getString("counteragent_fname"));
+					jon.put("performer",ibRequest.rs.getString("performer"));
+					jon.put("rightholder",ibRequest.rs.getString("rightholder"));
+					jon.put("subdog_pred",ibRequest.rs.getString("subdog_pred"));
+					jon.put("subdog_inProcess",""+ibRequest.rs.getString("subdog_inProcess"));
+					
 				arrayObj.put(jon);
 
 			}
@@ -203,6 +248,7 @@ System.out.println("============================================================
 	
 }
 
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
